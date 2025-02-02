7b22c57a662f096e12dccd64285da780e3feae4b
Rodrigo Toste Gomes <rodrigo.toste.gomes@gmail.com>
2025-02-01 23:30:20 -0800

    Get forks working and break a prophecy in a different fork

--- tva.py
+++ tva.py
@@ -163,9 +163,10 @@ class Environment:
         assert code_index < len(self.code_history)
 
         new_env = Environment()
-        new_env.code_history = env.code_history[:code_index + 1]
-        code = env.code_history[code_index]
-        new_env.var_histories = {var: history[:code.var_history_indexes[var] + 1] for var, history in env.var_histories.items()}
+        new_env.code_history = self.code_history[:code_index + 1]
+        code = self.code_history[code_index]
+        new_env.var_histories = {
+            var: history[:code.var_history_indexes[var] + 1] for var, history in self.var_histories.items() if var in code.var_history_indexes}
         new_env.var_histories[var_name][var_index].expression = new_value
         return new_env, code_index
     
@@ -177,8 +178,37 @@ class Environment:
             f"Code History:\n  [{",\n   ".join(str(elem) for elem in self.code_history)}]"
 
 def run_code(code, start_index=0, env=Environment()):
+    def resolve_prophecies_and_pending_forks(prev_code, next_code):
+        # Copy prophecies, but also try to resolve them.
+        for prophecy in prev_code.prophecies:
+            var, expression = prophecy
+            prophecy_value = expression.eval(env)
+            if prophecy_value is not None and var.name in env.var_histories and len(env.var_histories[var.name]) > var.index:
+                future_value = env.var_histories[var.name][var.index].expression.eval(env)
+                if future_value is not None:
+                    assert future_value == prophecy_value, "Prophecy violated."
+                    continue
+            if next_code is not None:
+                next_code.prophecies.append((var, prophecy_value or expression))
+        
+        # Check if any pending forks can be executed, or copy forward to try later.
+        for fork in prev_code.pending_forks:
+            fork_value = fork.right.eval(env)
+            if fork_value is not None:
+                new_env, code_index = env.fork(fork.left.name, fork.left.index, fork_value)
+                threading.Thread(target=run_code, args=(code, code_index, new_env)).start()
+            elif next_code is not None:
+                next_code.pending_forks.append(fork)
+
     for i, stmt in enumerate(code[start_index:]):
         next_code_history = CodeHistoryElement()
+
+        # Important that pending forks and prophecies get resolved before
+        # executing the stmt, otherwise a fork that breaks a prophecy would not
+        # get caught.
+        if len(env.code_history) != 0:
+            resolve_prophecies_and_pending_forks(env.code_history[-1], next_code_history)
+
         match stmt.kind:
             case Assignment.MUTATION:
                 assert stmt.left.index == 0 or \
@@ -208,28 +238,14 @@ def run_code(code, start_index=0, env=Environment()):
             case _:
                 assert False, "Invalid stmt kind."
 
-        if len(env.code_history) != 0:
-            # Copy prophecies, but also try to resolve them.
-            for prophecy in env.code_history[-1].prophecies:
-                var, expression = prophecy
-                prophecy_value = expression.eval(env)
-                if prophecy_value is not None and var.name in env.var_histories and len(env.var_histories[var.name]) > var.index:
-                    future_value = env.var_histories[var.name][var.index].expression.eval(env)
-                    if future_value is not None:
-                        assert future_value == prophecy_value, "Prophecy violated."
-                        print("PROPHECY FULLFILLED")
-                        continue
-                next_code_history.prophecies.append((var, prophecy_value or expression))
-            
-            # Check if any pending forks can be executed.
-            for fork in env.code_history[-1].pending_forks:
-                fork_value = fork.right.eval(env)
-                if fork_value is not None:
-                    new_env, code_index = env.fork(fork.left.name, fork.left.index, fork_value)
-                    threading.Thread(target=run_code, args=(code, code_index, new_env)).start()
-                else:
-                    next_code_history.pending_forks.append(fork)
+        # Now go and store all the current indexes
+        for var in env.var_histories:
+            next_code_history.var_history_indexes[var] = len(env.var_histories[var]) - 1
         env.code_history.append(next_code_history)
+
+    # Try one more time to resolve prophecies and pending forks.
+    if len(env.code_history) != 0:
+        resolve_prophecies_and_pending_forks(env.code_history[-1], None)
 
     print(env)
 
@@ -238,9 +254,11 @@ def run_code(code, start_index=0, env=Environment()):
 # x = 1
 # x:+1 = 2
 # x = 2
+# x:0 = 3
 code = [
     Assignment(Variable("x", 0), Literal(1, int), Assignment.MUTATION),
     Assignment(Variable("x", 1), Literal(2, int), Assignment.PROPHECY),
-    Assignment(Variable("x", 1), Literal(2, int), Assignment.MUTATION)]
+    Assignment(Variable("x", 1), Literal(2, int), Assignment.MUTATION),
+    Assignment(Variable("x", 1), Literal(3, int), Assignment.REVISION)]
 env = Environment()
 run_code(code)
