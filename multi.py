18197776e32d075d0fb31844bf382b43e68cd036
Kora <17602700+koragendum@users.noreply.github.com>
2025-02-02 18:06:43 -0800

    better interactive prompt

--- multi.py
+++ multi.py
@@ -2,15 +2,31 @@ from lexer  import *
 from parser import *
 from engine import *
 
+statements = []
+
 def prompt():
-    print("\x1B[2m>\x1B[22m", end=' ')
-    line = input()
-    if line in ['exit', 'quit']:
-        exit()
-    return line + "\n"
+    while True:
+        print("\x1B[2m>\x1B[22m", end=' ')
+        line = input()
+        stripped = line.strip()
+        if stripped in ('exit', 'quit'):
+            exit()
+        if stripped == 'show':
+            count = reindex(statements)
+            for s in statements:
+                print(s)
+            continue
+        if stripped == 'run':
+            count = reindex(statements)
+            out = {}
+            run_code_to_completion(statements, Environment(count), out)
+            for _, msgs in out.items():
+                for msg in msgs:
+                    print(msg)
+            continue
+        return line + "\n"
 
 stream = TokenStream("", prompt)
-statements = []
 
 while True:
     result = parse_statement(stream)
@@ -25,9 +41,3 @@ while True:
             reified.show(stream.log())
         else:
             statements.append(reified)
-            count = reindex(statements)
-            out = {}
-            run_code_to_completion(statements, Environment(count), out)
-            for _, msgs in out.items():
-                for msg in msgs:
-                    print(msg)
